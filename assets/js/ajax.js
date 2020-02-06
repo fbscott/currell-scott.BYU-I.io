38a932be5da98511ce27242af7a4de099b87b081
scott <scottcurrell@gmail.com>
2020-02-06 15:44:44 -0700

    working get high/low temps

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -37,10 +37,58 @@ WF.loadForecast = (url, loadCallback) => {
     _xhr.send();
 };
 
-WF.getDailyHigh = (data) => {};
+/******************************************************************************
+ * GET DAILY HIGH TEMP
+ * Takes an object array and a float parameter. Finds and returns the highest
+ * value for the parameter in the array.
+ * resource: getting max values in json array:
+ *           https://stackoverflow.com/questions/22949597/
+ *           getting-max-values-in-json-array#22949724
+ * @param  {Array} arr - object array
+ * @return {Float} highest temp found
+ *****************************************************************************/
+WF.getDailyHigh = (arr) => {
+    let _max;
 
-WF.getDailyLow = (data) => {};
+    for (var i = arr.list.length - 1; i >= 0; i--) {
+        if (_max === null || _max === undefined || arr.list[i].main.temp_max > _max) {
+            _max = arr.list[i].main.temp_max;
+        }
+    }
 
+    return _max;
+};
+
+/******************************************************************************
+ * GET DAILY LOW TEMP
+ * Takes an object array and a float parameter. Finds and returns the lowest
+ * value for the parameter in the array.
+ * resource: getting max values in json array:
+ *           https://stackoverflow.com/questions/22949597/
+ *           getting-max-values-in-json-array#22949724
+ * @param  {Array} arr - object array
+ * @return {Float} lowest temp found
+ *****************************************************************************/
+WF.getDailyLow = (arr) => {
+    let _low;
+
+    for (var i = arr.list.length - 1; i >= 0; i--) {
+        if (_low === null || _low === undefined || arr.list[i].main.temp_min < _low) {
+            _low = arr.list[i].main.temp_min;
+        }
+    }
+
+    return _low;
+};
+
+/******************************************************************************
+ * PARSE BY DAYS
+ * Takes a parsed JSON object and breaks it up by days (by the timestamp).
+ * resource: add a key value pair to a javascript object:
+ *           https://stackoverflow.com/questions/1168807/
+ *           how-can-i-add-a-key-value-pair-to-a-javascript-object#1168814
+ * @param  {Object} data - parsed JSON object array
+ *****************************************************************************/
 WF.parseByDays = (data) => {
     for (var i = 0; i < data.list.length; i++) {
         switch(new Date(data.list[i].dt * 1000).getDay()) {
@@ -84,6 +132,14 @@ WF.parseByDays = (data) => {
     }
 };
 
+WF.buildForecast = () => {
+    WF.forecast = [];
+
+    if (WF.sunday) {
+        WF.forecast.push({day: 'Sunday', high: WF.getDailyHigh(WF.sunday), low: WF.getDailyLow(WF.sunday)});
+    }
+};
+
 WF.updateTheDOM = (data) => {
     let _table = `<h2>Weather Forecast for ${data.city.name}</h2>
                     <table><tr>`;
@@ -101,13 +157,15 @@ WF.updateTheDOM = (data) => {
  *****************************************************************************/
 document.forecast.submit.addEventListener('click', function() {
     // DOM elements
-    WF.userInput = document.forecast.city.value;
+    WF.userInput         = document.forecast.city.value;
     WF.forecastContainer = document.getElementById('js-forecast-container');
+
     // API URI
     WF.baseURI = 'https://api.openweathermap.org/data/2.5/forecast';
     WF.city    = WF.userInput;
     WF.apiKey  = 'efb3e20ecbe6d6d8220230bbafc45108';
     WF.fullURI = WF.baseURI + '?q=' + WF.city + ',%20US,%20US&units=imperial&apiKey=' + WF.apiKey;
+
     // call the API and update the DOM
     WF.loadForecast(WF.fullURI, WF.updateTheDOM);
 });
