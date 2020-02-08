b0502b5d24a9d1e76f0b07ef5fa6c125aa5b6584
scott <scottcurrell@gmail.com>
2020-02-08 00:59:17 -0700

    working day parser - almost done - need to add current day weather

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -47,7 +47,7 @@ WF.loadForecast = (url, loadCallback) => {
  * @param  {Array} arr - object array
  * @return {Float} highest temp found
  *****************************************************************************/
-WF.getDailyHigh = (arr) => {
+WF.getDailyHigh = arr => {
     let _max;
 
     for (var i = arr.list.length - 1; i >= 0; i--) {
@@ -69,7 +69,7 @@ WF.getDailyHigh = (arr) => {
  * @param  {Array} arr - object array
  * @return {Float} lowest temp found
  *****************************************************************************/
-WF.getDailyLow = (arr) => {
+WF.getDailyLow = arr => {
     let _low;
 
     for (var i = arr.list.length - 1; i >= 0; i--) {
@@ -89,66 +89,104 @@ WF.getDailyLow = (arr) => {
  *           how-can-i-add-a-key-value-pair-to-a-javascript-object#1168814
  * @param  {Object} data - parsed JSON object array
  *****************************************************************************/
-WF.parseByDays = (data) => {
+WF.parseByDays = data => {
+    let _forecastStartDay = new Date(data.list[0].dt * 1000).getDay();
+
     for (var i = 0; i < data.list.length; i++) {
         switch(new Date(data.list[i].dt * 1000).getDay()) {
             case 0:
                 WF.sunday = WF.sunday || {};
                 WF.sunday.list = WF.sunday.list || [];
                 WF.sunday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['monday', 'tuesday', 'wednesday', 'thursday'];
+                }
                 break;
             case 1:
                 WF.monday = WF.monday || {};
                 WF.monday.list = WF.monday.list || [];
                 WF.monday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['tuesday', 'wednesday', 'thursday', 'friday'];
+                }
                 break;
             case 2:
                 WF.tuesday = WF.tuesday || {};
                 WF.tuesday.list = WF.tuesday.list || [];
                 WF.tuesday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['wednesday', 'thursday', 'friday', 'saturday'];
+                }
                 break;
             case 3:
                 WF.wednesday = WF.wednesday || {};
                 WF.wednesday.list = WF.wednesday.list || [];
                 WF.wednesday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['thursday', 'friday', 'saturday', 'sunday'];
+                }
                 break;
             case 4:
                 WF.thursday = WF.thursday || {};
                 WF.thursday.list = WF.thursday.list || [];
                 WF.thursday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['friday', 'saturday', 'sunday', 'monday'];
+                }
                 break;
             case 5:
                 WF.friday = WF.friday || {};
                 WF.friday.list = WF.friday.list || [];
                 WF.friday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['saturday', 'sunday', 'monday', 'tuesday'];
+                }
                 break;
             case 6:
                 WF.saturday = WF.saturday || {};
                 WF.saturday.list = WF.saturday.list || [];
                 WF.saturday.list.push({'dt': data.list[i].dt,'main': data.list[i].main, 'weather': data.list[i].weather});
+                if (new Date(data.list[i].dt * 1000).getDay() === _forecastStartDay) {
+                    WF.forecastCalendar = ['sunday', 'monday', 'tuesday', 'wednesday'];
+                }
                 break;
             default: break;
         }
     }
 };
 
-WF.buildForecast = () => {
-    WF.forecast = [];
-
-    if (WF.sunday) {
-        WF.forecast.push({day: 'Sunday', high: WF.getDailyHigh(WF.sunday), low: WF.getDailyLow(WF.sunday)});
-    }
-};
-
-WF.updateTheDOM = (data) => {
-    let _table = `<h2>Weather Forecast for ${data.city.name}</h2>
+WF.updateTheDOM = data => {
+    let _table = `<h2>5-Day Forecast for ${data.city.name}</h2>
                     <table><tr>`;
 
     WF.parseByDays(data);
 
+    for (var i = 0; i < WF.forecastCalendar.length; i++) {
+        _table += `<th>${(WF.forecastCalendar[i].toUpperCase())}</th>`;
+    }
+
+    _table += `</tr><tr>`;
+
+    for (var i = 0; i < WF.forecastCalendar.length; i++) {
+        _table += `<td>
+                     <p>${WF.getDailyHigh(WF[WF.forecastCalendar[i]])}</p>
+                     <p>${WF.getDailyLow(WF[WF.forecastCalendar[i]])}</p>
+                   </td>`;
+    }
+
     _table += `</tr></table>`;
 
     WF.forecastContainer.innerHTML = _table;
+};
+
+WF.clearData = () => {
+    WF.sunday = {};
+    WF.monday = {};
+    WF.tuesday = {};
+    WF.wednesday = {};
+    WF.thursday = {};
+    WF.friday = {};
+    WF.saturday = {};
 };
 
 /******************************************************************************
@@ -156,16 +194,23 @@ WF.updateTheDOM = (data) => {
  * Do all the things - kicks off the XMLHttpRequest, etc.
  *****************************************************************************/
 document.forecast.submit.addEventListener('click', function() {
+    WF.clearData();
+
     // DOM elements
     WF.userInput         = document.forecast.city.value;
     WF.forecastContainer = document.getElementById('js-forecast-container');
 
     // API URI
-    WF.baseURI = 'https://api.openweathermap.org/data/2.5/forecast';
+    WF.baseURI_weather  = 'https://api.openweathermap.org/data/2.5/weather';
+    WF.baseURI_forecast = 'https://api.openweathermap.org/data/2.5/forecast';
     WF.city    = WF.userInput;
     WF.apiKey  = 'efb3e20ecbe6d6d8220230bbafc45108';
-    WF.fullURI = WF.baseURI + '?q=' + WF.city + ',%20US,%20US&units=imperial&apiKey=' + WF.apiKey;
+    WF.fullURI_weather = WF.baseURI_weather + '?q=' + WF.city + ',%20US,%20US&units=imperial&apiKey=' + WF.apiKey;
+    WF.fullURI_forecast = WF.baseURI_forecast + '?q=' + WF.city + ',%20US,%20US&units=imperial&apiKey=' + WF.apiKey;
 
     // call the API and update the DOM
-    WF.loadForecast(WF.fullURI, WF.updateTheDOM);
+    WF.loadForecast(WF.fullURI_weather, function(data) {
+        // console.log(data);
+    });
+    WF.loadForecast(WF.fullURI_forecast, WF.updateTheDOM);
 });
