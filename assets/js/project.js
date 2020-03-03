6a5df2ef8b072a3fda0100a15476b445ca083a56
scott <scottcurrell@gmail.com>
2020-03-02 22:50:20 -0700

    fix grid layout and async

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,8 +7,9 @@ var PROJ = PROJ || {};
  * callback.
  *****************************************************************************/
 PROJ.ajax = (url, loadCallback) => {
+
     // placeholder while loading
-    // PROJ.forecastContainer.innerHTML = `<p>Loading weather forecast . . .</p>`;
+    PROJ.container.innerHTML = `<div class="column"><p>Loading weather forecast . . .</p></div>`;
 
     let _xhr = new XMLHttpRequest();
 
@@ -19,8 +20,8 @@ PROJ.ajax = (url, loadCallback) => {
 
             // pass the parsed response to the DOM loader
             loadCallback(_jsonObject);
-            PROJ.evenHeights();
             PROJ.flipPanels();
+            PROJ.evenHeights();
         }
     };
 
@@ -33,36 +34,50 @@ PROJ.ajax = (url, loadCallback) => {
  * @param  {Object} data - parsed JSON object array
  *****************************************************************************/
 PROJ.updateTheDOM = data => {
-    const _container = document.getElementById('js-panel-container');
-    let _cards       = '';
 
-    for (var i = 0; i < 3; i++) {
-        _cards += `
-          <div class="column large-4">
-            <div class="panel-container">
-              <div class="panel-flip js-panel-flip">
-                <div class="panel-front js-even-height">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div class="panel-back js-even-height">
-                  <img src="${data.hits[i].webformatURL}" />
-                </div>
-              </div>
-            </div>
-          </div>
-        `;
+    let _cards = `<div class="row">`;
+
+    for (var i = 0; i < 12; i++) {
+        if (i != 0 && i % 4 == 0) {
+            _cards += `</div><div class="row">`;
+        }
+
+        _cards += `<div class="column large-3">
+                     <div class="panel-container">
+                       <div class="panel-flip js-panel-flip">
+                         <div class="panel-front js-even-height">
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                         </div>
+                         <div class="panel-back js-even-height">
+                           <img src="${data.hits[i].webformatURL}" />
+                         </div>
+                       </div>
+                     </div>
+                   </div>`;
     }
 
-    _container.innerHTML = _cards;
+    _cards += `</div>`;
+
+    PROJ.container.innerHTML = _cards;
 };
 
+/******************************************************************************
+ * EVEN HEIGHTS
+ * Makes all panels the same height
+ *****************************************************************************/
 PROJ.evenHeights = () => {
-    const _panels    = document.getElementsByClassName('js-even-height');
+    const _flips = document.getElementsByClassName('js-panel-flip');
+    const _panels = document.getElementsByClassName('js-even-height');
     let _panelHeight = 0;
 
     // get largest panel height
     [].forEach.call(_panels, el => {
         if (el.offsetHeight > _panelHeight) { _panelHeight = el.offsetHeight }
+    });
+
+    // set all flips to the same height
+    [].forEach.call(_flips, el => {
+        el.setAttribute("style", "min-height:" + ( _panelHeight + 60 ) + "px");
     });
 
     // set all panels to the same height
@@ -71,6 +86,10 @@ PROJ.evenHeights = () => {
     });
 };
 
+/******************************************************************************
+ * PANEL FLIPPER
+ * Flips the panels over when clicked
+ *****************************************************************************/
 PROJ.flipPanels = () => {
     // all panels
     const _flippers = document.getElementsByClassName('js-panel-flip');
@@ -83,9 +102,21 @@ PROJ.flipPanels = () => {
     }
 };
 
+/******************************************************************************
+ * INITIALIZER
+ * Do all the things
+ *****************************************************************************/
 PROJ.init = () => {
 
-    PROJ.ajax('https://pixabay.com/api/?key=15462183-52054d7c5a68977efe09803b8&q=yellow+flowers&image_type=photo', PROJ.updateTheDOM);
+    PROJ.container = document.getElementById('js-panel-container');
+    PROJ.baseURL = 'https://pixabay.com/api/';
+    PROJ.key = '15462183-52054d7c5a68977efe09803b8';
+    PROJ.query = 'yellow+flowers';
+
+    PROJ.ajax(PROJ.baseURL + '?key=' +
+              PROJ.key + '&q=' +
+              PROJ.query + '&image_type=photo',
+              PROJ.updateTheDOM);
 
 };
 
